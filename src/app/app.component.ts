b78e55a00a8e12aa678e9f4981d0cde5603a2227
Paulo Melo <peimelo@gmail.com>
2020-09-29 11:56:12 -0300

    Login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from "@angular/core";
-import { LoadingService } from "./loading/loading.service";
-import { MessagesService } from "./messages/messages.service";
+import { Component, OnInit } from '@angular/core';
+import { AuthStore } from './services/auth.store';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
-  providers: [LoadingService, MessagesService],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  constructor() {}
+  constructor(public authStore: AuthStore) {}
 
   ngOnInit() {}
 
-  logout() {}
+  logout() {
+    this.authStore.logout();
+  }
 }
